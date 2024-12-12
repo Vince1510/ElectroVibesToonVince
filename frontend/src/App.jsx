64db392375cb6b8507b2ecc5f20f0dbb21b9befe
Toon van Berkel <toonvanberkel203@gmail.com>
2024-12-12 16:11:47 +0100

    checkout created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Admin from "./Pages/Admin/Admin";
 import BreadCrumb from "./components/BreadCrumbs";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
+import Checkout from "./pages/Checkout"
 import DetailPage from "./Pages/DetailPage";
 import Deals from './Pages/deals'
 import Compare from "./Pages/Compare";
@@ -27,6 +28,7 @@ function App() {
           <Route path="/product" element={<Product />} />
           <Route path="/detail/:category/:productId" element={<DetailPage />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="/deals" element={<Deals />} />
           <Route path="/compare" element={<Compare />} />
           <Route path="/login" element={<Login />} />
