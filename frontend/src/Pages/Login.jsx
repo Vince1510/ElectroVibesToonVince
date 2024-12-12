9cd2c6ff02447e48028b101a4f63e243684b8dae
Toon van Berkel <toonvanberkel203@gmail.com>
2024-12-12 16:00:46 +0100

    styled Login
    finished styling login

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,16 +1,119 @@
 import React from 'react';
-import { Typography, TextField, Button, Container } from '@mui/material';
+import { Typography, TextField, Button, Container, Box } from '@mui/material';
 
 function Login() {
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>Login</Typography>
-      <form>
-        <TextField label="Username" variant="outlined" fullWidth margin="normal" />
-        <TextField label="Password" type="password" variant="outlined" fullWidth margin="normal" />
-        <Button variant="contained" color="primary" fullWidth>Login</Button>
-      </form>
-    </Container>
+    <Box
+      sx={{
+        minHeight: '100vh',
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: 'linear-gradient(to bottom, #E70002, #000000, #FCD201)',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        zIndex: -1,
+      }}
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        }}
+      />
+      <Box
+        sx={{
+          zIndex: 1,
+          padding: 4,
+          borderRadius: 2,
+          width: '100%',
+          maxWidth: '800px',
+        }}
+      >
+        <Container sx={{ textAlign: 'center' }}>
+          <Typography
+            variant="h4"
+            gutterBottom
+            sx={{
+              color: '#FFFFFF',
+              fontFamily: 'Arial, sans-serif',
+              fontWeight: 'bold',
+            }}
+          >
+            ElectroVibe Inloggen
+          </Typography>
+          <form>
+            <TextField
+              label="Username/e-mailaddress"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              InputProps={{ sx: { color: '#FFFFFF', borderRadius: '5px' } }}
+              InputLabelProps={{ sx: { color: '#FFFFFF' } }}
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': {
+                    borderColor: '#FFFFFF',
+                    borderRadius: '5px',
+                  },
+                  '&:hover fieldset': {
+                    borderColor: '#FFFFFF',
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: '#FFFFFF',
+                  },
+                },
+              }}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              InputProps={{ sx: { color: '#FFFFFF', borderRadius: '5px' } }}
+              InputLabelProps={{ sx: { color: '#FFFFFF' } }}
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': {
+                    borderColor: '#FFFFFF',
+                    borderRadius: '5px',
+                  },
+                  '&:hover fieldset': {
+                    borderColor: '#FFFFFF',
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: '#FFFFFF',
+                  },
+                },
+              }}
+            />
+            <Button
+              variant="contained"
+              fullWidth
+              sx={{
+                backgroundColor: '#000000',
+                color: '#FFFFFF',
+                borderRadius: '5px',
+                fontWeight: 'bold',
+                mt: 4,
+                p: 1.5,
+                textTransform: 'none',
+                border: 'white 1px solid',
+              }}
+            >
+              Submit
+            </Button>
+          </form>
+        </Container>
+      </Box>
+    </Box>
   );
 }
 
